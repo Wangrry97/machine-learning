1d02b43c56741659ed9cd9892011b436d0cb9eed
wrry <wangrry@hotmail.com>
2023-04-11 10:59:35 +0800

    添加了一个python文件，用于测试

---
+++ test.py
@@ -0,0 +1,27 @@
+'''
+Author: wrry
+email: wangrry@hotmail.com
+Date: 2023-04-11 09:46:40
+LastEditors: wrry wangrry@hotmail.com
+LastEditTime: 2023-04-11 10:34:21
+FilePath: /machine-learning/test.py
+Description: to be written
+'''
+
+import numpy as np
+
+
+def docstring(a: str) -> str:
+    a = str(a)
+    return a
+
+
+# !
+# *
+# ?
+# todo
+# @param myparam
+
+if __name__ == '__main__':
+    a = np.zeros(3)
+    print(docstring(a))
