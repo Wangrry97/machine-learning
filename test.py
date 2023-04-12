5e3b64df8a6b1365fcb051874ebcb33229acc6ad
wrry <wangrry@hotmail.com>
2023-04-12 09:01:01 +0800

    conda env

--- test.py
+++ test.py
@@ -3,7 +3,7 @@ Author: wrry
 email: wangrry@hotmail.com
 Date: 2023-04-11 09:46:40
 LastEditors: wrry wangrry@hotmail.com
-LastEditTime: 2023-04-11 10:34:21
+LastEditTime: 2023-04-11 11:06:17
 FilePath: /machine-learning/test.py
 Description: to be written
 '''
